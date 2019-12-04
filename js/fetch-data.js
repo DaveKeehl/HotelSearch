eb4ab48281ae1f3727d69c61ef1b81cb9b7f8e76
Davide Ciulla <davide.ciulla@ticino.com>
2019-12-04 01:37:49 +0100

    small updates

--- js/fetch-data.js
+++ js/fetch-data.js
@@ -2,7 +2,7 @@ let searchField = document.getElementById("query");
 searchField.addEventListener("keydown", (event) => {
 	if (event.keyCode === 13) {
 		setMapOnAll(null);
-		composeRequest();
+		composeRequest(0);
 		document.activeElement.blur();
 		window.scrollTo(0, 0);
 	}
@@ -20,7 +20,7 @@ function composeRequest() {
 	let doubleSpaces = / +(?= )/g;
 	let spaceBeforePunctuation = / +(\W)/g;
 	let processedText = searchField.value.trim().replace(stopwords,'').replace(specialCharacters,'').replace(doubleSpaces,'').replace(spaceBeforePunctuation,'').replace(/ /g, "\%20OR\%20");
-	let rows = "&rows=5000";
+	let rows = "&rows=5000&start=0";
 	let requestFormat = "&wt=json";
 	var finalRequest = solrRequest + urlQuery + "(" + processedText + ")^2.0" + queryFieldSeparator + titleQuery + "(" + processedText + ")^1.5" + queryFieldSeparator + contentQuery + "(" + processedText + ")^1.0" + rows + requestFormat;
 	console.log(finalRequest);
@@ -67,13 +67,13 @@ function composeRequest() {
 						let skipResult = 0;
 						let urlRegex1 = /https?:\/\/([a-z0-9-]+\.)*tripadvisor\.co\.uk\/Hotel_Review([a-zA-Z0-9-])*-or([0-9])*-([a-zA-Z0-9-_])*/g;
 						let urlRegex2 = /https?:\/\/([a-z0-9-]+\.)*tripadvisor\.co\.uk\/Hotel_Review-s([0-9])-([a-zA-Z0-9-_])*/g;
-						console.log(url);
 						if (url.match(urlRegex1) || url.match(urlRegex2)) {
-							console.log("you don't want this link");
+							// console.log("you don't want this link");
 							skipResult = 1;
 							skippedCounter = skippedCounter + 1;
 						} else {
-							console.log("keep this");
+							// console.log(url);
+							// console.log("keep this");
 							keepCounter = keepCounter + 1;
 						}
 						let address;
@@ -169,14 +169,14 @@ function composeRequest() {
 						if (keepCounter < 20) {
 							// console.log("result number: " + i);
 							if (wordsInTitle[0] === "THE" && wordsInTitle[1] === "10" && wordsInTitle[2] === "BEST") {
-								console.log("found a TOP 10 list... skipping");
+								// console.log("found a TOP 10 list... skipping");
 							} else {
 								// console.log(hotel);
 								// console.log(url);
 								// console.log(address);
 								// console.log(phoneNumber);
 								// console.log(hotelDescription);
-								console.log("skipResult: " + skipResult);
+								// console.log("skipResult: " + skipResult);
 								if ((typeof doc.title !== 'undefined' || typeof doc.content !== '') && skipResult === 0) {
 									codeAddress(i, hotel, url, address, phoneNumber, hotelDescription, score, vote, propertyAmenities, roomFeatures, roomTypes, languageSpoken);
 								}
@@ -215,6 +215,7 @@ function composeRequest() {
 				if (`${data.response.numFound}` > 0) {
 					numberOfResults = `${data.response.numFound}` - skippedCounter;
 					console.log("Skipped results: " + skippedCounter);
+					console.log("Results shown: " + `${data.response.numFound}` + "-" + skippedCounter + " = " + numberOfResults);
 				} else {
 					numberOfResults = 0;
 				}
@@ -230,9 +231,18 @@ function composeRequest() {
 					`;
 				}
 			})
-			.catch(error => console.log(error))
+			.catch(error => {
+				console.log(error);
+				output = `
+					<div id="nothing-to-show">
+						<h1>Ouch... Is Solr running on localhost:8983?</h1>
+						<img src="src/images/robot.png">
+					</div>
+				`;
+				document.getElementById("results").innerHTML = output;
+			})
 	} else {
-		var output = `
+		output = `
 			<div id="nothing-to-show">
 				<h1>Please enter a query.</h1>
 				<img src="src/images/robot.png">
